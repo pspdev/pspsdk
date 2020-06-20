f4531908d15a9a90180fb4f2b899d1905a9a3403
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2020-06-20 05:31:04 -0400

    Add clang_psp.h

---
+++ src/base/clang_psp.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#if !defined(__ASM__) && !defined(__ASSEMBLY__) && !defined(__ASSEMBLER__)
+extern float __builtin_allegrex_ceil_w_s(float);
+extern float __builtin_allegrex_floor_w_s(float);
+extern float __builtin_allegrex_trunc_w_s(float);
+extern float __builtin_allegrex_round_w_s(float);
+extern float __builtin_allegrex_sqrt_s(float);
+#endif
