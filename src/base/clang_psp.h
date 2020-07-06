05afff09081abb4b86678e81484eb65dfd33e58d
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2020-07-06 07:49:03 -0400

    Redefine __UINTPTR_TYPE__ for clang
    It seems to be erroneously set to `unsigned long int` by default. While
    this type technically has the same number of bits as `unsigned int` on
    this target, the semantics are different.

--- src/base/clang_psp.h
+++ src/base/clang_psp.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#undef __UINTPTR_TYPE__
+#define __UINTPTR_TYPE__ unsigned int
+
 #if !defined(__ASM__) && !defined(__ASSEMBLY__) && !defined(__ASSEMBLER__)
 extern float __builtin_allegrex_ceil_w_s(float);
 extern float __builtin_allegrex_floor_w_s(float);
