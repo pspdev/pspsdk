39a161f9271e8110c63e28ad1c41572ab08122e4
balika011 <balika011@gmail.com>
2021-12-06 04:10:35 +0100

    pspnand

--- src/nand/pspnand_driver.h
+++ src/nand/pspnand_driver.h
@@ -18,6 +18,12 @@
 extern "C" {
 #endif
 
+enum PspNandDriverErrorCodes 
+{
+	SCE_NAND_ERROR_READ_ECCFAILED = 0x80230003,
+	SCE_NAND_ERROR_READEXTRA_ECCFAILED = 0x80230009,
+};
+
 int sceNandSetWriteProtect(int protectFlag);
 
 int sceNandLock(int writeFlag);
@@ -30,18 +36,29 @@ int sceNandReset(int flag);
 
 int sceNandReadId(void *buf, SceSize size);
 
-int sceNandReadPages(u32 ppn, void *buf, void *buf2, u32 count);
+int sceNandReadPages(u32 ppn, void *buf, void *spare, u32 count);
 
-/*
-// sceNandWritePages
-// sceNandReadAccess
-// sceNandWriteAccess
-// sceNandEraseBlock
-// sceNandReadExtraOnly
-// sceNandCalcEcc
-// sceNandVerifyEcc
-// sceNandCollectEcc
-*/
+int sceNandReadExtraOnly(u32 ppn, void *buf, u32 count);
+
+int sceNandReadPagesRawAll(u32 ppn, void *buf, void *spare, u32 count);
+
+int sceNandReadAccess(u32 ppn, void *buf, void *spare, u32 count, u32 flags);
+
+int sceNandWritePages(u32 ppn, void *buf, void *spare, u32 count);
+
+int sceNandWriteAccess(u32 ppn, void *buf, void *spare, u32 count, u32 flags);
+
+int sceNandReadAccess(u32 ppn, void *buf, void *spare, u32 count, u32 flags);
+
+int sceNandEraseBlock(u32 ppn);
+
+int sceNandReadExtraOnly(u32 ppn, void *spare, u32 len);
+
+int sceNandCalcEcc(void *buf);
+
+int sceNandVerifyEcc(void *buf, u16 ecc);
+
+int sceNandCollectEcc(void *buf, u16 ecc); 
 
 int sceNandGetPageSize(void);
 
@@ -49,24 +66,21 @@ int sceNandGetPagesPerBlock(void);
 
 int sceNandGetTotalBlocks(void);
 
-/*
-// sceNandWriteBlock
-// sceNandWriteBlockWithVerify
-*/
+int sceNandWriteBlock(u32 ppn, void *buf, void *spare);
+
+int sceNandWriteBlockWithVerify(u32 ppn, void *buf, void *spare);
 
 int sceNandReadBlockWithRetry(u32 ppn, void *buf, void *buf2);
 
-/*
-// sceNandVerifyBlockWithRetry
-// sceNandEraseBlockWithRetry
-*/
+int sceNandVerifyBlockWithRetry(u32 ppn, void *buf, void *spare);
+
+int sceNandEraseBlockWithRetry(u32 ppn);
 
 int sceNandIsBadBlock(u32 ppn);
 
-/*
-// sceNandEraseAllBlock
-// sceNandTestBlock
-*/
+int sceNandEraseAllBlock(void);
+
+int sceNandTestBlock(u32 ppn);
 
 #ifdef __cplusplus
 }
