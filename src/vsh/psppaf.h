a533ae1e8db82d4f4c559b737850d65a49a4c4bb
balika011 <balika011@gmail.com>
2022-04-04 22:02:29 +0200

    paf

--- src/vsh/psppaf.h
+++ src/vsh/psppaf.h
@@ -41,6 +41,91 @@ void *paf_memcpy(void *s1, const void *s2, int n);
 */
 void *paf_memset(void *s, int c, int n);
 
+/**
+ * strtok_r
+*/
+char *paf_strtok_r(char *str, const char *delim, char **saveptr);
+
+/**
+ * malloc
+*/
+void *paf_malloc(int size);
+
+/**
+ * free
+*/
+void paf_free(void *);
+
+/**
+ * 
+*/
+int paf_wcslen(wchar_t *);
+
+/**
+ * strncpy
+*/
+char *paf_strncpy(char *, const char *, int);
+
+/**
+ * strcmp
+*/
+int paf_strcmp(const char *, const char *);
+
+/**
+ * strncmp
+*/
+int paf_strncmp(const char *, const char *, int);
+
+/**
+ * strchr
+*/
+char *paf_strchr(const char *, int);
+
+/**
+ * strrchr
+*/
+char *paf_strrchr(const char *, int);
+
+/**
+ * strpbrk
+*/
+char *paf_strpbrk(const char *, const char *);
+
+/**
+ * strtoul
+*/
+int paf_strtoul(const char *, char **, int);
+
+/**
+ * scePafGetText
+*/
+wchar_t *scePafGetText(void *, char *);
+
+typedef struct
+{
+	u8 id;
+	u8 type;
+	u16 unk1;
+	u32 label;
+	u32 param;
+	u32 first_child;
+	int child_count;
+	u32 next_entry;
+	u32 prev_entry;
+	u32 parent;
+	u32 unknown[2];
+} SceRcoEntry;
+
+/**
+ * PAF_Resource_GetPageNodeByID
+*/
+int PAF_Resource_GetPageNodeByID(void *, char *, SceRcoEntry **);
+
+/**
+ * PAF_Resource_ResolveRefWString
+*/
+int PAF_Resource_ResolveRefWString(void *, u32 *, int *, char **, int *);
+
 #endif
 
 
