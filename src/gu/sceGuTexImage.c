fcf9ed6d3584823843783afddc903aa0f7d29c05
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-09-15 12:02:13 -0700

    gu: Add clang-friendly codepath

--- src/gu/sceGuTexImage.c
+++ src/gu/sceGuTexImage.c
@@ -14,14 +14,15 @@ static int tsizecmd_tbl[8] = { 0xb8, 0xb9, 0xba, 0xbb, 0xbc, 0xbd, 0xbe, 0xbf };
 
 int getExp(int val)
 {
+#ifndef __clang__
 	unsigned int i;
 	asm("clz %0, %1\n":"=r"(i):"r"(val&0x3FF));
 	return 31-i;
-/*
+#else
 	unsigned int i;
 	for (i = 9; (i > 0) && !((val >> i) & 1); --i);
 	return i;
-*/
+#endif
 }
 
 void sceGuTexImage(int mipmap, int width, int height, int tbw, const void* tbp)
