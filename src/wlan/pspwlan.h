38e6ade7ae862ebf1e756502ea981118b919e16a
Wally4000 <catapultit@me.com>
2024-11-01 09:44:41 +1100

    Use uint8_t instead of referring to psptypes

--- src/wlan/pspwlan.h
+++ src/wlan/pspwlan.h
@@ -11,7 +11,6 @@
 #ifndef __PSPWLAN_H__
 #define __PSPWLAN_H__
 
-#include <psptypes.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -34,11 +33,11 @@ int sceWlanGetSwitchState(void);
 /**
  * Get the Ethernet Address of the wlan controller
  *
- * @param etherAddr - pointer to a buffer of u8 (NOTE: it only writes to 6 bytes, but 
+ * @param etherAddr - pointer to a buffer of uint8_t (NOTE: it only writes to 6 bytes, but 
  * requests 8 so pass it 8 bytes just in case)
  * @return 0 on success, < 0 on error
  */
-int sceWlanGetEtherAddr(u8 *etherAddr);
+int sceWlanGetEtherAddr(uint8_t *etherAddr);
 
 /**
  * Attach to the wlan device
