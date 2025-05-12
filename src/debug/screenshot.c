fbdb62c71f35d679d33d0420eeab5c4a9df8ede6
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2025-05-12 22:26:28 +0200

    Create function header

---
+++ src/debug/screenshot.c
@@ -0,0 +1,17 @@
+/*
+ * PSP Software Development Kit - https://github.com/pspdev
+ * -----------------------------------------------------------------------
+ * Licensed under the BSD license, see LICENSE in PSPSDK root for details.
+ *
+ *  screenshot.c - Simple screen debug keyboard
+ *
+ * Copyright (c) 2025, Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
+ *
+ */
+
+#include "pspdebug.h"
+
+int pspScreenshotSave(const char *filename)
+{
+    return 0;
+}
