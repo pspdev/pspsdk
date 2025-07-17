4a24ecd2e5be48f66a1036c49972223c56d48684
fjtrujy <fjtrujy@users.noreply.github.com>
2025-07-17 22:37:13 +0000

    deploy: b3f0fb5f5029cc3a6085a9b489509c647963b0d8

---
+++ structPspUsbCamSetupStillParam.js
@@ -0,0 +1,9 @@
+var structPspUsbCamSetupStillParam =
+[
+    [ "complevel", "structPspUsbCamSetupStillParam.html#ad0229f3031c2197b419f835a99fab4d2", null ],
+    [ "delay", "structPspUsbCamSetupStillParam.html#a2ea72a356800657469062ab259b46bf0", null ],
+    [ "jpegsize", "structPspUsbCamSetupStillParam.html#a6721e42c05b21bdee0316027a854e12f", null ],
+    [ "resolution", "structPspUsbCamSetupStillParam.html#a134e799034de223de4196c00d7c70fc5", null ],
+    [ "reverseflags", "structPspUsbCamSetupStillParam.html#a3d6486146f2ee62bf05f3f1b49acae5c", null ],
+    [ "size", "structPspUsbCamSetupStillParam.html#ab67d8d5a426dc619654896534645f1fa", null ]
+];
