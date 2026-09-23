cc6ecbba5c06c96263934641cbcf3303c50b6158
Daniel Welch <dwelch.appleid@gmail.com>
2026-09-23 12:05:49 +0100

    remove scratchpad thread attribute
    This thread attribute has been illegal on PSP firmware 1.00, 1.50 and 6.60 as proven by disassembly. It's never worked and simply never been documented.
    Scratchpad can be written to manually, but threads created with `PSP_THREAD_ATTR_SCRATCH_SRAM` have never succeeded.

--- src/user/pspthreadman.h
+++ src/user/pspthreadman.h
@@ -53,8 +53,6 @@ enum PspThreadAttributes
 	PSP_THREAD_ATTR_USBWLAN = 0xa0000000,
 	/** Thread is part of the VSH API. */
 	PSP_THREAD_ATTR_VSH = 0xc0000000,
-	/** Allow using scratchpad memory for a thread, NOT USABLE ON V1.0 */
-	PSP_THREAD_ATTR_SCRATCH_SRAM = 0x00008000,
 	/** Disables filling the stack with 0xFF on creation */
 	PSP_THREAD_ATTR_NO_FILLSTACK = 0x00100000,
 	/** Clear the stack when the thread is deleted */
